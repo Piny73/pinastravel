9a2ddf55b886e4969227f96af2c7e3922856ab57
Giuseppina Salemme <Piny73@gmail.com>
2024-06-25 10:37:46 +0200

    aggio' Winviaggi

--- src/main/java/it/tsp/JDBC/Partecipants.java
+++ src/main/java/it/tsp/JDBC/Partecipants.java
@@ -1,16 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package it.tsp.JDBC;
 
-/**
- *
- * @author utentepc
- */
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+package it.tsp.JDBC;
 
 public class Partecipants {
     private Long idPartecipante;
@@ -47,50 +36,11 @@ public class Partecipants {
         this.idViaggio = idViaggio;
     }
 
-    // Metodi per l'insert, update e select
-    public static void insert(Partecipants partecipants) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
-        PreparedStatement stmt = conn.prepareStatement("INSERT INTO partecipants (id_partecipante, id_accoun_turista, id_viaggio) VALUES (?, ?, ?)");
-        stmt.setLong(1, partecipants.getIdPartecipante());
-        stmt.setLong(2, partecipants.getIdAccounTurista());
-        stmt.setLong(3, partecipants.getIdViaggio());
-        stmt.executeUpdate();
-        conn.close();
-    }
-
-    public static List<Partecipants> selectAll() throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM partecipants");
-        List<Partecipants> partecipantsList = new ArrayList<>();
-        while (rs.next()) {
-            Partecipants partecipant = new Partecipants(
-                    rs.getLong("id_partecipante"),
-                    rs.getLong("id_accoun_turista"),
-                    rs.getLong("id_viaggio")
-            );
-            partecipantsList.add(partecipant);
-        }
-        conn.close();
-        return partecipantsList;
-    }
-
-    public static void update(Partecipants partecipants) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
-        PreparedStatement stmt = conn.prepareStatement("UPDATE partecipants SET id_partecipante=?, id_accoun_turista=?, id_viaggio=? WHERE id_partecipante=?");
-        stmt.setLong(1, partecipants.getIdPartecipante());
-        stmt.setLong(2, partecipants.getIdAccounTurista());
-        stmt.setLong(3, partecipants.getIdViaggio());
-        stmt.setLong(4, partecipants.getIdPartecipante());
-        stmt.executeUpdate();
-        conn.close();
-    }
-
-    public static void delete(Partecipants partecipants) throws SQLException {
+    /*public static void delete(Partecipants partecipants) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
         PreparedStatement stmt = conn.prepareStatement("DELETE FROM partecipants WHERE id_partecipante=?");
         stmt.setLong(1, partecipants.getIdPartecipante());
         stmt.executeUpdate();
         conn.close();
-    }
+    } */
 }
